562b1511e2f9c7eaebd91a2987fcec9f02ed3136
Александр Давыдов <davidov.abeslab@gmail.com>
2025-08-01 07:24:20 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <ColorSwatch name=

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,6 +56,13 @@ export default {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
         },
+        'chart-1': 'hsl(var(--chart-1))',
+        'chart-2': 'hsl(var(--chart-2))',
+        'chart-3': 'hsl(var(--chart-3))',
+        'chart-4': 'hsl(var(--chart-4))',
+        'chart-5': 'hsl(var(--chart-5))',
+        'chart-6': 'hsl(var(--chart-6))',
+        'chart-7': 'hsl(var(--chart-7))',
       },
       borderRadius: {
         lg: 'var(--radius)',
